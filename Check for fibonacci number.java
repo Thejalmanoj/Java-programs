c2ac5aad3759d70b072c39ea03fb6536854af539
Thejal <76029279+Thejalmanoj@users.noreply.github.com>
2024-01-09 21:47:47 +0530

    Create Check for fibonacci number.java

---
+++ Check for fibonacci number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main
+{
+    //create a method to check for fibonacci number//
+    public static boolean checkFibonacci(int n)
+    {
+        int a=0,b=1,c=1;
+        while(c<n)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return (c==n);
+    }
+  //create a main method to take user inputs and to call function//
+	public static void main(String[] args)
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the number:");
+    //read the input from user and store it in variable num//
+		int num=in.nextInt();
+    System.out.println((checkFibonacci(num)?"fibonacci number":"not a fibonacci number"));
+	}
+}
